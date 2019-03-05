bcdeb092bce4a16dc3b98d1919d96d69d5ca5aef
Prashant Sutar <psutar@wk.pune>
2019-03-05 14:26:33 +0530

    Authentication, Employee Dir, Internal openings

--- src/app/core/components/nav-bar/nav-bar.component.ts
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthenticationService }  from '../../../services/authentication.service';
+import { DataService }  from '../../../services/data.service';
+import { PostDataModel } from '../../../models/PostDataModel';
+
 @Component({
   selector: 'wk-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
+  private authRslt: string = '';
+  private authBack: string = 'grey';
+  
+  constructor( private authSvc: AuthenticationService) { }
 
   ngOnInit() {
+    var result = this.authSvc.getUser().subscribe(
+      r => {this.authRslt = JSON.parse(r); this.authBack = 'success';},
+      e => {console.log(e); this.authBack = 'error';}
+    );
+   
   }
 
 }
